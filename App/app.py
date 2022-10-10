6b6f06a87d7d72c9abdc4e82df6d6b043c1f2e69
Kunaljadhav2229 <93173983+Kunaljadhav2229@users.noreply.github.com>
2022-10-11 01:27:52 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle
 import string
 import nltk
-nltk.download(stopwords)
 from nltk.corpus import stopwords
 import os
 os.chdir(r"C:\Users\Admin\Desktop\SMS_EMAIL_SPAM_Detector_web_app\App")
